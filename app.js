44159c676f7388e791895d8056c8c530d2c5cf98
Aditya Danturthi <aditya2096@gmail.com>
2023-05-06 18:31:00 -0230

    button styling

--- app.js
+++ app.js
@@ -7,20 +7,21 @@ const inputTask = document.getElementById('input-task');
 // Event listener for add task button
 addTask.addEventListener('click', function(){
     let task = document.createElement('div');
-    task.classList.add('task');
+    task.classList.add('task', 'text-black', 'w-full', 'bg-white','p-5', 'mb-2', 'flex', 'justify-between');
 
     let li = document.createElement('li');
     li.innerText = `${inputTask.value}`;
+    li.classList.add('flex-auto', 'list-none');
     task.appendChild(li);
 
     let checkButton = document.createElement('button');
     checkButton.innerHTML = '<i class="fa-solid fa-check"></i>';
-    checkButton.classList.add('checkTask');
+    checkButton.classList.add('checkTask', 'flex-1', 'rounded-lg', 'text-green-500', 'mr-2', 'border-2', 'text-base', 'border-green-500');
     task.appendChild(checkButton);
 
     let deleteButton = document.createElement('button');
     deleteButton.innerHTML = '<i class="fa-solid fa-trash"></i>';
-    deleteButton.classList.add('deleteTask');
+    deleteButton.classList.add('deleteTask', 'flex-1', 'rounded-lg', 'text-red-500', 'border-2', 'text-base', 'border-red-500');
     task.appendChild(deleteButton);
 
     if(inputTask.value === ''){
@@ -28,4 +29,5 @@ addTask.addEventListener('click', function(){
     } else {
         taskContainer.appendChild(task);
     }
+    inputTask.value = "";
 });
