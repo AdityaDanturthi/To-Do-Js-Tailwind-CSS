b74b9ea7f06bebeef28913989594dd73ca758a4b
Aditya Danturthi <aditya2096@gmail.com>
2023-05-06 19:19:22 -0230

    check button is working

--- app.js
+++ app.js
@@ -7,21 +7,21 @@ const inputTask = document.getElementById('input-task');
 // Event listener for add task button
 addTask.addEventListener('click', function(){
     let task = document.createElement('div');
-    task.classList.add('task', 'text-black', 'w-full', 'bg-white','p-5', 'mb-2', 'flex', 'justify-between');
+    task.classList.add('task', 'text-black', 'w-full', 'bg-white','p-4', 'mb-2', 'flex', 'justify-between');
 
     let li = document.createElement('li');
     li.innerText = `${inputTask.value}`;
-    li.classList.add('flex-auto', 'list-none');
+    li.classList.add('flex-auto', 'list-none', 'text-xl');
     task.appendChild(li);
 
     let checkButton = document.createElement('button');
     checkButton.innerHTML = '<i class="fa-solid fa-check"></i>';
-    checkButton.classList.add('checkTask', 'flex-1', 'rounded-lg', 'text-green-500', 'mr-2', 'border-2', 'text-base', 'border-green-500');
+    checkButton.classList.add('checkTask', 'flex-1', 'rounded-lg', 'text-green-500', 'mr-2', 'border-2', 'text-base', 'border-green-500', 'hover:bg-green-500', 'hover:text-white', 'text-xl');
     task.appendChild(checkButton);
 
     let deleteButton = document.createElement('button');
     deleteButton.innerHTML = '<i class="fa-solid fa-trash"></i>';
-    deleteButton.classList.add('deleteTask', 'flex-1', 'rounded-lg', 'text-red-500', 'border-2', 'text-base', 'border-red-500');
+    deleteButton.classList.add('deleteTask', 'flex-1', 'rounded-lg', 'text-red-500', 'border-2', 'text-base', 'border-red-500', 'hover:bg-red-500', 'hover:text-white', 'text-xl');
     task.appendChild(deleteButton);
 
     if(inputTask.value === ''){
@@ -30,4 +30,8 @@ addTask.addEventListener('click', function(){
         taskContainer.appendChild(task);
     }
     inputTask.value = "";
+
+    checkButton.addEventListener('click', function(){
+        li.classList.add('line-through');
+    })
 });
